b71c3e426732b8e1d76e656a5e3642ee170313aa
Adan Mendoza <adan@fractalframing.com>
2020-06-19 12:27:51 -0600

    build on local

---
+++ build/precache-manifest.1255463e960acb3bdac731f85b91b477.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b237a6392726656dd2282c9a3e3a712e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8bb343280483faca47a",
+    "url": "/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "447cbe8bdd1af0f29879",
+    "url": "/static/css/main.794ef11b.chunk.css"
+  },
+  {
+    "revision": "d8bb343280483faca47a",
+    "url": "/static/js/2.fdab93c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fdab93c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "447cbe8bdd1af0f29879",
+    "url": "/static/js/main.1fdb0993.chunk.js"
+  },
+  {
+    "revision": "6144e6f352c6528250b3",
+    "url": "/static/js/runtime-main.cd8ca6b7.js"
+  }
+]);
